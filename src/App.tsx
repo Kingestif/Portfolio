be30dc4660057284ee2d2b64578fb3b3629120b1
Kingestif <kingestiff@gmail.com>
2025-08-13 08:58:12 +0300

    feat: add parallex animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App'>
+      <Parallax pages={2} style={{top:'0', left: '0'}} className="animation">
+        <ParallaxLayer offset={0} speed={0.25}>
+          <div className='animation_layer' id='background'></div>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={0} speed={0.3+0.1}>
+          <div className='animation_layer' id='mountains'></div>
+        </ParallaxLayer>
+        
+        <ParallaxLayer offset={0} speed={0.3+0.3}>
+          <div className='animation_layer' id='jungle1'></div>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={0} speed={0.35+0.3}>
+          <div className='animation_layer' id='jungle2'></div>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={0} speed={0.5+0.3}>
+          <div className='animation_layer' id='jungle3'></div>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={0} speed={0.45+0.7}>
+          <div className='animation_layer' id='jungle4'></div>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={0} speed={0.40}>
+          <div className='animation_layer' id='man_on_mountain'></div>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={0} speed={0.35+1.5}>
+          <div className='animation_layer' id='jungle5'></div>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={0.95} speed={0}>
+          <div className='animation_layer' id='land_logo'></div>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={1} speed={0}>
+          <div className='dark_page' id='dark'></div>
+        </ParallaxLayer>
+
+      </Parallax>
+    </div>
   )
 }
 

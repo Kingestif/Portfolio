6733f97c208b955c3a653acdb94864df998d9078
Kingestif <kingestiff@gmail.com>
2025-08-14 15:49:27 +0300

    refactor: made project height dynamic using state

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,36 @@ import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 import About from './components/About'
 import { Navbar } from './components/Navbar'
 import { Projects } from './components/Projects'
+import { useEffect, useRef, useState } from 'react'
 
 function App() {
+  //below snippet b/c my Project component excceded 1 page so we need its exact height
+  const projectsRef = useRef<HTMLDivElement>(null)
+  const [projectsFactor, setProjectsFactor] = useState<number | null>(null)
+
+  useEffect(() => {
+    if (projectsRef.current) {
+      const height = projectsRef.current.getBoundingClientRect().height
+      setProjectsFactor(height / window.innerHeight)
+    }
+  }, [])
+
+  if (projectsFactor === null) {
+    return (
+      <div style={{ visibility: 'hidden', position: 'absolute' }}>
+        <div ref={projectsRef}>
+          <Projects />
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div className='App'>
       <Navbar/>
       
-      <Parallax pages={3} style={{top:'0', left: '0'}} className="animation">
-        
+      <Parallax pages={3 + projectsFactor} style={{top:'0', left: '0'}} className="animation">
+
         <ParallaxLayer offset={0} speed={0.1}>
           <div className='animation_layer' id='background'></div>
         </ParallaxLayer>
@@ -19,10 +41,6 @@ function App() {
           <div className='animation_layer' id='mountains'></div>
         </ParallaxLayer>
 
-        {/* <ParallaxLayer offset={0} speed={-0.5}>
-          <div className='animation_layer' id='logoland'></div>
-        </ParallaxLayer> */}
-        
         <ParallaxLayer offset={0} speed={0.2}>
           <div className='animation_layer' id='jungle1'></div>
         </ParallaxLayer>
@@ -55,8 +73,12 @@ function App() {
           <About/>
         </ParallaxLayer>
 
-        <ParallaxLayer offset={2} speed={0}>
+        <ParallaxLayer offset={2} speed={0} factor={projectsFactor}>
           <Projects/>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={2 + projectsFactor} speed={0}>
+          <div className='h-screen bg-blue-500'>Hellow</div>
         </ParallaxLayer>
 
       </Parallax>

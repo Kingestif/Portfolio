c605eb2e4837b924a4d73f95392e97cdbbb1fc21
Kingestif <kingestiff@gmail.com>
2025-08-15 15:14:57 +0300

    feat: add separate experience section

---
+++ src/utils/experience.tsx
@@ -0,0 +1,37 @@
+export const experiences = [
+  {
+    id: 4,
+    title: "Backend Developer",
+    company: "Afriwork",
+    duration: "(Jun 2025 - Present)",
+    description:
+      "Built automated code review and testing system that analyzes code quality, detect issues and integrates an automated testing framework into CI/CD pipelines, reducing review and testing time by 40%.",
+    link: "https://afriworket.com/",
+  },
+  {
+    id: 3,
+    title: "Software Engineer Intern",
+    company: "ICog",
+    duration: "(Feb 2025 - Aug 2025)",
+    description:
+      "Trained machine learning models, developed recommendation systems and predictive models, and implemented 5+ logic-based programs in MeTTa within the ECAN team to simulate intelligent agent behavior and attention-based decision-making.",
+    link: "https://icog-labs.com/",
+  },
+  {
+    id: 2,
+    title: "Software Engineer Intern",
+    company: "Eskalate",
+    duration: "(Jul 2024 - Oct 2024)",
+    description:
+      "Led a team of 5 to develop a Flutter E-commerce app with 1,000+ products, implementing key features and improving delivery speed by 25% through effective sprint management and collaboration.",
+    link: "https://eskalate.io/",
+  },
+  {
+    id: 1,
+    title: "Freelance Developer",
+    company: "Self Employed",
+    duration: "(Mar 2024 - Present)",
+    description:
+      "Delivering full-stack freelance projects using modern web technologies. Building custom solutions and maintaining client relationships.",
+  },
+];

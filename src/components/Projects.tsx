25c71352d09c6e1167f921ebef05bd50386ac0ad
Kingestif <kingestiff@gmail.com>
2025-08-16 10:48:08 +0300

    refactor: ui fix on project

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -10,7 +10,7 @@ export function Projects(){
             </div>
             <div className="flex gap-10 items-center">
                 {projects.map(project=>(
-                    <div key={project.id} className="flex flex-col w-110 h-110 items-center text-white gap-3 rounded-2xl bg-gray-500/25">
+                    <div key={project.id} className="flex flex-col w-110 h-110 items-center text-white gap-3 rounded-2xl bg-gray-500/25 hover:scale-105 transition-all duration-200 ease-out shadow-sm  hover:bg-gray-500/30">
                         <img src={project.imageUrl} alt={project.title} className="rounded-t-2xl overflow-hidden" />
                         <div className="flex justify-around w-full pt-2">
                             {project.tools.map((tool,idx)=>(
@@ -21,15 +21,15 @@ export function Projects(){
                         <div className="w-full text-center text-gray-300 px-5">{project.description}</div>
                         <div className="flex-1"></div>
                         <div className="flex gap-5 self-start text-gray-400 px-5 py-3">
-                            <a href={project.liveUrl} target="_blank" rel="noopener noreferrer"><ExternalLink/></a>
-                            <a href={project.githubUrl} target="_blank" rel="noopener noreferrer"><Github/></a>
+                            <a href={project.liveUrl} target="_blank" rel="noopener noreferrer" className="hover:text-white duration-500"><ExternalLink/></a>
+                            <a href={project.githubUrl} target="_blank" rel="noopener noreferrer" className="hover:text-white"><Github/></a>
                         </div>
                     </div>
                 ))}
             </div>
-            <a href="https://github.com/Kingestif" target="_blank" rel="noopener noreferrer" className="flex gap-2 bg-yellow-700 rounded-full py-2 px-3">
-                <div className="text-white font-bold">Check my Github</div>
-                <div className="text-white"><ArrowRight /></div>
+            <a href="https://github.com/Kingestif" target="_blank" rel="noopener noreferrer" className="group flex gap-2 bg-yellow-500/ rounded-full py-2 px-3 ">
+                <div className="text-white font-bold">Check my <span className="text-yellow-500">Github</span></div>
+                <div className="text-yellow-500 w-5 h-5 group-hover:translate-x-2 duration-300"><ArrowRight /></div>
             </a>
         </div>
     )
--- src/utils/projects.tsx
+++ src/utils/projects.tsx
@@ -5,7 +5,7 @@ export const projects = [
     description:
       "AI-powered CV ranking system that enables recruiters to efficiently shortlist top candidates.",
     imageUrl: "/CVision.png",
-    tools: ["React", "Node", "Express"],
+    tools: ["React", "Node", "Express", "FastAPI"],
     githubUrl: "https://github.com/Kingestif/cv-ranker",
     liveUrl: "https://cvisionio.vercel.app/",
   },
@@ -15,7 +15,7 @@ export const projects = [
     description:
       "E-commerce application that enables real-time price comparison and personalized product recommendations.",
     imageUrl: "/TalakKinash.png",
-    tools: ["React", "Node", "Express"],
+    tools: ["Node", "Express", "MongoDB"],
     githubUrl: "https://github.com/Kingestif/Talak-kinash-backend",
     liveUrl: "https://talak-kinash.vercel.app/",
   },
@@ -25,7 +25,7 @@ export const projects = [
     description:
       "Restaurant API that streamlines menu management, orders, and reservations, helping restaurants operate more efficiently.",
     imageUrl: "/Food2.png",
-    tools: ["React", "Node", "Express"],
+    tools: ["Flutter", "Node", "Express"],
     githubUrl: "https://github.com/Kingestif/Restaurant-API",
     liveUrl: "https://github.com/Kingestif/FoodApp",
   },

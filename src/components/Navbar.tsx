cd3bc53a6e778c2e0dff8b377dd468c7d5c3d1c3
Kingestif <kingestiff@gmail.com>
2025-08-16 10:47:52 +0300

    refactor: made full name

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 export function Navbar(){
     return (
         <div className="navbar flex justify-between py-3 px-5 text-xl font-bold text-yellow-500 bg-black">
-            <div>Estif</div>
+            <div>Estifanos Zinabu</div>
             <div className="flex gap-5">
                 <div>Home</div>
                 <div>Services</div>

02043f913b54f4c47ba0d0862a6350552d5c4c9d
Kingestif <kingestiff@gmail.com>
2025-08-23 22:53:55 +0300

    fix: remove parallax as it it so slow and not practical

--- src/App.tsx
+++ src/App.tsx
@@ -1,93 +1,18 @@
 import './App.css'
-import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 import About from './components/About'
 import { Navbar } from './components/Navbar'
 import { Experience } from './components/Experience'
-import { useEffect, useRef, useState } from 'react'
 import { Projects } from './components/Projects'
 import { Contact } from './components/Contact'
 
 function App() {
-  //below snippet b/c my Project component excceded 1 page so we need its exact height
-  const projectsRef = useRef<HTMLDivElement>(null)
-  const [projectsFactor, setProjectsFactor] = useState<number | null>(null)
-
-  useEffect(() => {
-    if (projectsRef.current) {
-      const height = projectsRef.current.getBoundingClientRect().height
-      setProjectsFactor(height / window.innerHeight)
-    }
-  }, [])
-
-  if (projectsFactor === null) {
-    return (
-      <div style={{ visibility: 'hidden', position: 'absolute' }}>
-        <div ref={projectsRef}>
-          <Experience />
-        </div>
-      </div>
-    )
-  }
-
   return (
     <div className='App'>
       <Navbar/>
-      
-      <Parallax pages={4 + projectsFactor} style={{top:'0', left: '0'}} className="animation">
-
-        <ParallaxLayer offset={0} speed={0.1}>
-          <div className='animation_layer' id='background'></div>
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={0} speed={0.15}>
-          <div className='animation_layer' id='mountains'></div>
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={0} speed={0.2}>
-          <div className='animation_layer' id='jungle1'></div>
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={0} speed={0.2}>
-          <div className='animation_layer' id='jungle2'></div>
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={0} speed={0.3}>
-          <div className='animation_layer' id='jungle3'></div>
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={0} speed={0.5}>
-          <div className='animation_layer' id='jungle4'></div>
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={0} speed={0.15}>
-          <div className='animation_layer parallax' id='man_on_mountain'></div>
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={0} speed={1}>
-          <div className='animation_layer' id='jungle5'></div>
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={0.6} speed={0.85}>
-          <div className='animation_layer' id='black'></div>
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={1} speed={0}>
-          <About/>
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={2} speed={0} factor={projectsFactor}>
-          <Experience/>
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={2 + projectsFactor} speed={0}>
-          <Projects/>
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={3 + projectsFactor} speed={0}>
-          <Contact/>
-        </ParallaxLayer>
-
-      </Parallax>
+      <About/>
+      <Experience/>
+      <Projects/>
+      <Contact/>
     </div>
   )
 }

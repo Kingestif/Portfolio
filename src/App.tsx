6529b482222ff6a304088bcfcf51eb78a13a0c6c
Kingestif <kingestiff@gmail.com>
2025-08-25 23:18:41 +0300

    feat: add skill section

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Experience } from './components/Experience'
 import { Projects } from './components/Projects'
 import { Contact } from './components/Contact'
 import { Hero } from './components/Hero'
+import { Skills } from './components/Skills'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Navbar/>
       <Hero/>
       <About/>
+      <Skills/>
       <Experience/>
       {/* <Projects/> */}
       {/* <Contact/> */}

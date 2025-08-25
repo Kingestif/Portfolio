da57a6d56e9003ab1702452de2d06c48fcf43743
Kingestif <kingestiff@gmail.com>
2025-08-26 00:37:16 +0300

    refactor: ui

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -11,14 +11,6 @@ function About() {
                     </div>
                     <div className="text-xl"> Software Developer with over 3 years of experience specializing in Frontend, Backend, and Mobile App Development. I can design and build scalable, user-friendly applications from concept to deployment, leveraging modern technologies and clean architecture principles. My greatest strength is the ability to quickly adapt to new tools and frameworks, ensuring high-quality, maintainable solutions while collaborating effectively across multidisciplinary teams. </div>
                 </div>
-
-                {/* <div className="w-fit group flex gap-2 text-yellow-500 text-xl">
-                    <div className="relative font-medium after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-0 after:bg-yellow-500 after:transition-all after:duration-300 group-hover:after:w-full">
-                        Download Resume
-                    </div>
-                    <Download />
-                </div> */}
-
             </div>
 
             <img className="bg-gradient-to-t from-violet-800 to-purple-500 rounded-full w-100 h-100 object-cover" src="/Me.webp" alt="" />

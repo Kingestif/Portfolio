7e0486fdc8475de8d6d5063d1c6345c57ac75d3c
Kingestif <kingestiff@gmail.com>
2025-08-26 16:25:10 +0300

    feat: add responsiveness on hero section & navbar

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,10 +3,10 @@ import { Download } from "lucide-react";
 export function Hero(){
     return (
         <div className="pt-40 flex  justify-between text-white/90">
-            <div className="Left w-1/2 flex flex-col gap-10 pt-7">
-                <div className="text-6xl font-medium">Estifanos Zinabu</div>
-                <div className="text-4xl text-[color:#6C63FF]">Full-Stack Developer</div>
-                <div className="text-xl text-gray-300">Full-Stack Software Engineer specializing in building scalable web and mobile applications. I create seamless, user-friendly, and high-performance digital solutions.</div>
+            <div className="Left w-1/2 flex flex-col gap-10 pt-7 | max-sm:w-fit">
+                <div className="text-6xl font-medium | max-sm:text-xl w-fit max-sm:whitespace-nowrap ">Estifanos Zinabu</div>
+                <div className="text-4xl text-[color:#6C63FF] | w-fit max-sm:whitespace-nowrap max-sm:text-xl">Full-Stack Developer</div>
+                <div className="text-xl text-gray-300 | max-sm:text-sm max-sm:w-full w-fit">Full-Stack Software Engineer specializing in building scalable web and mobile applications. I create seamless, user-friendly, and high-performance digital solutions.</div>
                 <div className="flex gap-10">
                     <a href="https://github.com/Kingestif" target="_blank" className="p-0.5 bg-gradient-to-t from-pink-700 to-purple-600 rounded-full hover:scale-110 duration-300">
                         <svg className="w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path d="M237.9 461.4C237.9 463.4 235.6 465 232.7 465C229.4 465.3 227.1 463.7 227.1 461.4C227.1 459.4 229.4 457.8 232.3 457.8C235.3 457.5 237.9 459.1 237.9 461.4zM206.8 456.9C206.1 458.9 208.1 461.2 211.1 461.8C213.7 462.8 216.7 461.8 217.3 459.8C217.9 457.8 216 455.5 213 454.6C210.4 453.9 207.5 454.9 206.8 456.9zM251 455.2C248.1 455.9 246.1 457.8 246.4 460.1C246.7 462.1 249.3 463.4 252.3 462.7C255.2 462 257.2 460.1 256.9 458.1C256.6 456.2 253.9 454.9 251 455.2zM316.8 72C178.1 72 72 177.3 72 316C72 426.9 141.8 521.8 241.5 555.2C254.3 557.5 258.8 549.6 258.8 543.1C258.8 536.9 258.5 502.7 258.5 481.7C258.5 481.7 188.5 496.7 173.8 451.9C173.8 451.9 162.4 422.8 146 415.3C146 415.3 123.1 399.6 147.6 399.9C147.6 399.9 172.5 401.9 186.2 425.7C208.1 464.3 244.8 453.2 259.1 446.6C261.4 430.6 267.9 419.5 275.1 412.9C219.2 406.7 162.8 398.6 162.8 302.4C162.8 274.9 170.4 261.1 186.4 243.5C183.8 237 175.3 210.2 189 175.6C209.9 169.1 258 202.6 258 202.6C278 197 299.5 194.1 320.8 194.1C342.1 194.1 363.6 197 383.6 202.6C383.6 202.6 431.7 169 452.6 175.6C466.3 210.3 457.8 237 455.2 243.5C471.2 261.2 481 275 481 302.4C481 398.9 422.1 406.6 366.2 412.9C375.4 420.8 383.2 435.8 383.2 459.3C383.2 493 382.9 534.7 382.9 542.9C382.9 549.4 387.5 557.3 400.2 555C500.2 521.8 568 426.9 568 316C568 177.3 455.5 72 316.8 72zM169.2 416.9C167.9 417.9 168.2 420.2 169.9 422.1C171.5 423.7 173.8 424.4 175.1 423.1C176.4 422.1 176.1 419.8 174.4 417.9C172.8 416.3 170.5 415.6 169.2 416.9zM158.4 408.8C157.7 410.1 158.7 411.7 160.7 412.7C162.3 413.7 164.3 413.4 165 412C165.7 410.7 164.7 409.1 162.7 408.1C160.7 407.5 159.1 407.8 158.4 408.8zM190.8 444.4C189.2 445.7 189.8 448.7 192.1 450.6C194.4 452.9 197.3 453.2 198.6 451.6C199.9 450.3 199.3 447.3 197.3 445.4C195.1 443.1 192.1 442.8 190.8 444.4zM179.4 429.7C177.8 430.7 177.8 433.3 179.4 435.6C181 437.9 183.7 438.9 185 437.9C186.6 436.6 186.6 434 185 431.7C183.6 429.4 181 428.4 179.4 429.7z"/></svg>
@@ -25,15 +25,15 @@ export function Hero(){
                     </a>
                     
                 </div>
-                <div className="mt-10 hover:text-white duration-300 flex gap-10 text-xl text-white/80">
+                <div className="mt-2xl hover:text-white duration-300 flex gap-10 text-xl text-white/80 whitespace-nowrap | max-sm:text-base max-sm:whitespace-nowrap">
                     <a href="https://drive.google.com/uc?export=download&id=1hHg3_dsT1oDl-31QYdp0jTaS0W6_a6RW" className="group flex gap-3 items-center rounded-full py-3 px-4 border-2 border-purple-500/50">
                         <div className="">Get Resume</div>
                         <Download className="group-hover:ml-2 duration-300"/>
                     </a>
-                    <a href="#contact" className="rounded-full py-3 px-4 flex items-center bg-gradient-to-t from-pink-700 to-purple-600 hover:text-white duration-300">Contact Me</a>
+                    <a href="#contact" className="  rounded-full py-3 px-4 flex items-center bg-gradient-to-t from-pink-700 to-purple-600 hover:text-white duration-300">Contact Me</a>
                 </div>
             </div>
-            <img className="Right w-170 h-170   " src="/HeroRight.svg" alt="" />
+            <img className="Right w-170 h-170 | max-sm:hidden" src="/HeroRight.svg" alt="" />
         </div>
     )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,9 @@
 export function Navbar(){
     return (
-        <div className="navbar flex justify-between text-xl">
-            <div className="text-[color:#6C63FF] text-3xl font-medium">Estifanos Zinabu</div>
+        <div className="navbar flex justify-between text-xl | max-sm:flex-col max-sm:text-base">
+            <div className="text-[color:#6C63FF] text-3xl font-medium | max-sm:hidden">Estifanos Zinabu</div>
             <div className="flex gap-5 text-white/80">
-                <a href="#hero" className="hover:text-[color:#6C63FF] duration-300">Home</a>
+                <a href="#hero" className="hover:text-[color:#6C63FF] duration-300 max-sm:text-blue-400">Home</a>
                 <a href="#about" className="hover:text-[color:#6C63FF] duration-300">About</a>
                 <a href="#experience" className="hover:text-[color:#6C63FF] duration-300">Experience</a>
                 <a href="#projects" className="hover:text-[color:#6C63FF] duration-300">Projects</a>

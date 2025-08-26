0500d4e120d7bc5aac2135f09fd582d57b10c719
Kingestif <kingestiff@gmail.com>
2025-08-26 12:14:59 +0300

    refactor: link navbars to their specific page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,13 @@
 export function Navbar(){
     return (
-        <div className="navbar flex justify-between text-2xl text-white/90">
-            <div className="text-purple-600 text-3xl">Estifanos Zinabu</div>
-            <div className="flex gap-5">
-                <div>Home</div>
-                <div>Services</div>
-                <div>Projects</div>
-                <div>Contact</div>
+        <div className="navbar flex justify-between text-xl">
+            <div className="text-[color:#6C63FF] text-3xl font-medium">Estifanos Zinabu</div>
+            <div className="flex gap-5 text-white/80">
+                <a href="#hero" className="hover:text-[color:#6C63FF] duration-300">Home</a>
+                <a href="#about" className="hover:text-[color:#6C63FF] duration-300">About</a>
+                <a href="#experience" className="hover:text-[color:#6C63FF] duration-300">Experience</a>
+                <a href="#projects" className="hover:text-[color:#6C63FF] duration-300">Projects</a>
+                <a href="#contact" className="hover:text-[color:#6C63FF] duration-300">Contacts</a>
             </div>
         </div>
     )

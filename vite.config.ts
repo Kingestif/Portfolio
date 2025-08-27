afc9bb9901f341f26b773120d4a5f7e2cdac65e9
Kingestif <kingestiff@gmail.com>
2025-08-27 12:13:39 +0300

    fix: update build target to chrome90

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: { target: "es2017" },
+  build: { target: "chrome90" },
 });

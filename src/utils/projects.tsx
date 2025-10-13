d9956df2889674bf19756ee32e8d6ec8e3ebcb7e
Kingestif <kingestiff@gmail.com>
2025-10-13 16:20:45 +0300

    feat: adde new projects

--- src/utils/projects.tsx
+++ src/utils/projects.tsx
@@ -1,6 +1,17 @@
 export const projects = [
   {
-    id: 1,
+    id:1,
+    title: "Trailwatch",
+    description: 
+      "Movie trailer discovery site built with Next.js that lets you explore trending films, watch trailers, and TV shows.",
+    imageUrl: "/TrailwatchBeth.png",
+    tools: ["NextJS", "ReactJS","TailwindCSS"],
+    githubUrl: "https://github.com/Kingestif/Trailwatch",
+    liveUrl: "https://trailwatch-mu.vercel.app/"
+  },
+
+  {
+    id: 2,
     title: "CVision",
     description:
       "AI-powered CV ranking system that enables recruiters to efficiently shortlist top candidates.",
@@ -9,24 +20,36 @@ export const projects = [
     githubUrl: "https://github.com/Kingestif/cv-ranker",
     liveUrl: "https://cvisionio.vercel.app/",
   },
-  {
-    id:2,
-    title: "Talak-Kinash",
-    description:
-      "E-commerce application that enables real-time price comparison and personalized product recommendations.",
-    imageUrl: "/TalakKinash.png",
-    tools: ["Node", "Express", "MongoDB"],
-    githubUrl: "https://github.com/Kingestif/Talak-kinash-backend",
-    liveUrl: "https://talak-kinash.vercel.app/",
-  },
+
   {
     id:3,
-    title: "Restaurant-API",
-    description:
-      "Restaurant API that streamlines menu management, orders, and reservations, helping restaurants operate more efficiently.",
-    imageUrl: "/Food2.png",
-    tools: ["Flutter", "Node", "Express"],
-    githubUrl: "https://github.com/Kingestif/Restaurant-API",
-    liveUrl: "https://github.com/Kingestif/FoodApp",
-  },
+    title: "Bankify",
+    description: 
+      "Banking dashboard that provides a seamless digital banking experience with beautiful UI, interactive charts, and real-time account management",
+    imageUrl: "/Bankify1.png",
+    tools: ["NextJS", "ReactJS","CSS"],
+    githubUrl: "https://github.com/Kingestif/Bankify?tab=readme-ov-file",
+    liveUrl: "https://bankify-seven.vercel.app/"
+  }
+  
+  // {
+  //   id:2,
+  //   title: "Talak-Kinash",
+  //   description:
+  //     "E-commerce application that enables real-time price comparison and personalized product recommendations.",
+  //   imageUrl: "/TalakKinash.png",
+  //   tools: ["Node", "Express", "MongoDB"],
+  //   githubUrl: "https://github.com/Kingestif/Talak-kinash-backend",
+  //   liveUrl: "https://talak-kinash.vercel.app/",
+  // },
+  // {
+  //   id:3,
+  //   title: "Restaurant-API",
+  //   description:
+  //     "Restaurant API that streamlines menu management, orders, and reservations, helping restaurants operate more efficiently.",
+  //   imageUrl: "/Food2.png",
+  //   tools: ["Flutter", "Node", "Express"],
+  //   githubUrl: "https://github.com/Kingestif/Restaurant-API",
+  //   liveUrl: "https://github.com/Kingestif/FoodApp",
+  // },
 ];
